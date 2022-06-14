fa7d302d5b2b06646e8194b810c64eee72a5fa6f
Hessam Imen <hessam.imen@gmail.com>
2022-06-14 14:26:37 +1000

    Done with assignmnet

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
   # your code here
+  sqArray=[]
+  array.each do |num|
+      sqArray.push(num**2)
+  end
+  return sqArray
 end
+
+#implement this with  ".collect"
+
+# def square_array(array)
+#   # your code here
+#   newArr = array.collect do |num|
+#       num**2
+#     end
+#     puts newArr.inspect
+# end
+
+# square_array([1,2])
